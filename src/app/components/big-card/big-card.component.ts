07228f771558d95ea82438f6ec47ebd9392cc216
unknown <ariane.maranha@outlook.com>
2023-03-31 21:01:41 -0300

    components dynamics

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: [
+    './big-card.component.css',
+    './big-card.responsive.component.css',
+  ],
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
 
+  //#region bound data
+  @Input() coverPhoto: string = '';
+  @Input() cardTitle: string = '';
+  @Input() cardDescription: string = '';
+  //#endregion
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+
+  @Input() cardPhoto:string='';
+  @Input() cardTitle:string='';
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
